e3931297bf2fc2aa967ba4a7aa08c76cf1d0695a
wolede <oluwawolede.adeniyi@venturegardengroup.com>
2019-05-13 17:14:28 +0100

    Firstest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { random } from 'lodash';
+import { StarWarsService } from './star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dreamOfMine';
+  number: number;
+  list: any = ['apples', 'oranges', 'stuff']
+  item: any;
+  constructor(public swService: StarWarsService) {}
+
+  ngOnInit(){
+    this.swService.fetchCharacters();
+  }
+
+  onIncrease(){
+    this.number = random(1, 20);
+  }
+  addItem(){
+    this.list.push(this.item);
+  }
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { StarWarsService } from '../star-wars.service';
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+  @Input() character;
+  // @Output() sideAssigned = new EventEmitter<{ name: string, side: string }>();
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+  ngOnInit() {
+  }
+
+  onAssign(side) {
+    // this.character.side = side;
+    this.swService.onSideChosen({name: this.character.name, side: side});
+
+  }
+
+}
